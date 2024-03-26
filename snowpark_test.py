7a7ceaca60997ee06462d29bc46dafbfc58fd2e2
Vikas-2005-Sharma <vikas.sharma@tigeranalytics.com>
2024-03-26 13:27:37 +0530

    Update snowpark_test.py

--- snowpark_test.py
+++ snowpark_test.py
@@ -2,7 +2,6 @@ from snowflake.snowpark.session import Session
 from snowflake.snowpark.session import Session 
 from snowflake.snowpark import DataFrame 
 from snowflake.snowpark.functions import col
-from config import snowflake_conn_prop_local as snowflake_conn_prop
 
 def hello(session: Session) -> DataFrame:
     df = session.table("demodb.dev.customers")
@@ -11,5 +10,13 @@ def hello(session: Session) -> DataFrame:
 
 # For local debugging
 if __name__ == "__main__":
-    session = Session.builder.configs(snowflake_conn_prop).create()
+    session = Session.builder.configs( {
+   "account": "jyvwqoq-lr08285",
+   "user": "Vikas2005S",
+   "password": "Prophet1$",
+   "database": "TEST_DB",
+   "schema": "TEST_SCHEMA",
+   "warehouse": "COMPUTE_WH",
+   "role":"ACCOUNTADMIN",
+}).create()
     print (hello (session).show())
